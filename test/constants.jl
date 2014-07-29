373e7d483778ae58804f0d24da362c570e0d3afe
Matthieu Laneuville <mlaneuville@elsi.jp>
2014-07-29 15:50:23 +0900

    Updated test/constfile

--- test/constants.jl
+++ test/constants.jl
@@ -1,6 +1,7 @@
 #constants
-const NB = 10 # number of biofilms
+const NB = 5 # number of biofilms
 const NC = 20 # number of cells per biofilm
-const GL = 10 # number of genes in a genome
+const GL = 5 # number of genes in a genome
 const FF = 5  # number of functions
-const MAXTIME = 10.
+const MF = 0.05 # mutation factor
+const MAXTIME = 10
