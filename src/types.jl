a50b9520512f3fb23c8d55402f6832ca507fa2a7
Matthieu Laneuville <mlaneuville@elsi.jp>
2014-07-29 10:41:04 +0900

    sketch of longer term view

--- src/types.jl
+++ src/types.jl
@@ -21,7 +21,22 @@ Cell() = Cell(GL)
 type Biofilm
     individuals::Array{Cell,1}
 
-    Biofilm(size::Integer) = new([Cell() for i in [1:size]])
+    function Biofilm(size::Integer) = new([Cell() for i in [1:size]])
+end
+
+b = Biofilm()
+a = grow(b)
+
+function grow(bf::Biofilm)
+    sporesize = 3
+    spore = Biofilm(sporesize)
+    
+    for i in [1:NC-sporesize]
+        # choose cell at random
+        idx = rand(1:ncells+i-1)
+        # duplicate
+        bf.individuals[ncells+i]
+    end
 end
 
 Biofilm() = Biofilm(NC)
--- test/constants.jl
+++ test/constants.jl
@@ -3,3 +3,4 @@ const NB = 10 # number of biofilms
 const NC = 20 # number of cells per biofilm
 const GL = 10 # number of genes in a genome
 const FF = 5  # number of functions
+const MAXTIME = 10.
