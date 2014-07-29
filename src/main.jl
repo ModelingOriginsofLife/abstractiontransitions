3f5dcab49e17b3cd44092d5edd85c81349b37614
Matthieu Laneuville <mlaneuville@elsi.jp>
2014-07-29 11:58:14 +0900

    Added fitness and new_gen sampling

--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,8 @@
 require("../test/constants.jl")
 require("types.jl")
 
+using StatsBase
+
 # population = Biofilm(5)
 # numfuncs = map(x->sum(x.genome),population.individuals)
 # totalfuncs = reduce((x,y)->x+y,map(x->x.genome,population.individuals))
@@ -15,11 +17,11 @@ require("types.jl")
 #----------------------------------
 
 # should initialize NB fully developed biofilms
-pop = Population(NB)
+pop = Population(3)
 
-# time loop
-for time
-    for bf in pop.individuals
-        
-    end
-end
+display(pop.individuals[1])
+
+fitvec = (Float64)[x.fitness for x in pop.individuals]
+
+new_gen = sample(pop.individuals, WeightVec(fitvec), length(pop.individuals))
+pop.individuals = new_gen
--- src/types.jl
+++ src/types.jl
@@ -2,9 +2,11 @@
 
 using Distributions
 
+######################################################
 type Cell
     genome::BitArray{1}
     promoter::Array{Integer,1}
+    fitness::Float64
 
     function Cell(genomelength::Integer)
         dist = Categorical([.7,.2,.1])
@@ -12,16 +14,33 @@ type Cell
         inds = rand(1:genomelength,numgenes)
         genebitstring = falses(genomelength)
         genebitstring[inds] = true
-        return new(genebitstring, rand(1:genomelength,genomelength))
+        fitness = getfitness(genebitstring)
+        new(genebitstring, rand(1:genomelength,genomelength), fitness)
     end
+end
+
+function getfitness(gen::BitArray{1})
+    sum(gen)
 end
 
 Cell() = Cell(GL)
 
+######################################################
 type Biofilm
     individuals::Array{Cell,1}
+    fitness::Float64
 
-    Biofilm(size::Integer) = new([Cell() for i in [1:size]])
+    function Biofilm(size::Integer) 
+        cells = [Cell() for i in [1:size]]
+        fitness = getfitness(cells)
+        new(cells, fitness)
+    end
+end
+
+function getfitness(cells::Array{Cell,1})
+    s = reduce((x,y)->x+y, map(x->x.fitness, cells))
+    s *= (0.5 + rand())
+    s 
 end
 
 function grow(bf::Biofilm)
@@ -31,11 +50,21 @@ function grow(bf::Biofilm)
         newcell = deepcopy(bf.individuals[idx])
         push!(bf.individuals, newcell)
     end
+    bf.fitness = getfitness(bf.individuals)
+end
+
+function display(bf::Biofilm)
+    println("Number of cells: ", length(bf.individuals))
+    println("Total fitness: ", bf.fitness)
+    for cell in bf.individuals
+        println("- ", cell.genome, ", fitness: ",    cell.fitness)
+    end
 end
 
 
 Biofilm() = Biofilm(NC)
 
+######################################################
 type Population
     individuals::Array{Biofilm,1}
 
