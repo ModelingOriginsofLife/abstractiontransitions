5cb5b81d3e958af94431789d93b962ae9a6aaf1c
Matthieu Laneuville <mlaneuville@elsi.jp>
2014-07-29 15:38:41 +0900

    Code cleanup and commenting

--- src/types.jl
+++ src/types.jl
@@ -3,6 +3,11 @@
 using Distributions
 
 ######################################################
+# Definition of the Cell type
+# - Cell(), Cell(genomelength)
+# - getfitness(genome)
+# - mutate(Cell)
+######################################################
 type Cell
     genome::BitArray{1}
     promoter::Array{Integer,1}
@@ -19,20 +24,33 @@ type Cell
     end
 end
 
+Cell() = Cell(GL)
+
 function getfitness(gen::BitArray{1})
     sum(gen)
 end
 
 function mutate(c::Cell)
-    factor = 0.05
-    idx = [x < y for (x,y) in zip(rand(GL), factor*ones(GL))]
+    idx = [x < y for (x,y) in zip(rand(GL), MF*ones(GL))]
     c.genome[idx] = !c.genome[idx]
     c.fitness = getfitness(c.genome)
 end
 
+# Display properties of a cell
+function display(c::Cell)
+    println("Genome: ", c.genome)
+    println("Promoter: ", c.promoter)
+    println("Fitness: ", c.fitness)
+end
 
-Cell() = Cell(GL)
-
+######################################################
+# Definition of the Biofilm type
+# - Biofilm(), Biofilm(ncells), Biofilm(CellArray)
+# - getfitness(CellArray)
+# - grow(Biofilm)
+# - display(Biofilm)
+# - reproduce(Biofilm)
+# - getspore(Biofilm)
 ######################################################
 type Biofilm
     individuals::Array{Cell,1}
@@ -50,12 +68,14 @@ type Biofilm
     end
 end
 
+Biofilm() = Biofilm(NC)
+
 function getfitness(cells::Array{Cell,1})
     s = reduce((x,y)->x+y, map(x->x.fitness, cells))
-    #s *= (0.5 + rand())
-    s 
 end
 
+# Complete cells a biofilm by duplicating cells of the spore
+# A mutation factor is included
 function grow(bf::Biofilm)
     ncells = length(bf.individuals)
     for i in range(1, NC-ncells)
@@ -67,6 +87,7 @@ function grow(bf::Biofilm)
     bf.fitness = getfitness(bf.individuals)
 end
 
+# Display properties of a biofilm
 function display(bf::Biofilm)
     println("Number of cells: ", length(bf.individuals))
     println("Total fitness: ", bf.fitness)
@@ -75,6 +96,7 @@ function display(bf::Biofilm)
     end
 end
 
+# Create a spore and grow it
 function reproduce(bf::Biofilm)
     idx = getspore(bf)
     new_ = Biofilm(bf.individuals[idx])
@@ -82,6 +104,7 @@ function reproduce(bf::Biofilm)
     new_
 end
 
+# Returns a set of cells which possess all possible functions
 function getspore(bf::Biofilm)
     survival = falses(GL)
     idx = sample([1:NC], NC, replace=false)
@@ -100,8 +123,9 @@ function getspore(bf::Biofilm)
     return idx[1:j]
 end
 
-Biofilm() = Biofilm(NC)
-
+######################################################
+# Definition of the Population type
+# - Population(), Population(nfilms)
 ######################################################
 type Population
     individuals::Array{Biofilm,1}
