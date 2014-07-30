c3e216a2d118db08a9a8df217b8ff70e2402e8ef
Cameron Smith <camrn86@gmail.com>
2014-07-30 14:17:51 +0900

    move includes into separate file for use in repl

--- src/main.jl
+++ src/main.jl
@@ -1,19 +1,7 @@
 using StatsBase
 using Datetime
 
-# configfile = "constants.jl"
-configfile = "constants_test.jl"
-indir = joinpath("..","input")
-outdir = joinpath("..","output")
-require(joinpath(indir,configfile))
-
-require("utilities.jl")
-require("types.jl")
-require("cell.jl")
-require("biofilm.jl")
-require("population.jl")
-require("measure.jl")
-require("plot.jl")
+require("include.jl")
 
 #-------------------------
 # setup
