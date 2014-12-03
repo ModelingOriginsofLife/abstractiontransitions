668d91794a8c45bedf03b403f0985b06630e7453
Cameron Smith <camrn86@gmail.com>
2014-12-03 17:51:19 -0500

    make function imports explicit in Probmodel.hs

--- analysis/probmodel/Probmodel.hs
+++ analysis/probmodel/Probmodel.hs
@@ -2,14 +2,14 @@
 
 module Main where
 
-import System.Environment
+import System.Environment(getArgs)
 import Prelude
-import Numeric.LinearAlgebra as LA
-import Data.MultiSet as MS
-import Data.List as DL
-import Data.List.HT as HT
-import Control.Monad as CM
-import System.Random
+import Numeric.LinearAlgebra(Matrix,RandDist(Uniform),(><),dispf,reshape,randomVector,trans)
+import Data.MultiSet(Occur,fromList,occur)
+import Data.List(concatMap,filter,map,transpose)
+import Data.List.HT(sliceVertical)
+import Control.Monad(replicateM)
+import System.Random(randomIO)
 
 {-|
   main function
@@ -47,7 +47,7 @@ randMat r c = do
   [[True,True],[True,False],[False,True],[False,False]]
 -}
 boolLists :: Int -> [[Bool]]
-boolLists g = CM.replicateM g [True,False]
+boolLists g = replicateM g [True,False]
 
 {-|
   The 'combsWithRep' function takes an argument of type 'Int' and a list and generates all combinations from elements of the list of size given by the first arguments
@@ -59,7 +59,7 @@ combsWithRep :: Int -> [a] -> [[a]]
 combsWithRep k xs = combsBySize xs !! k
  where
    combsBySize = foldr f ([[]] : repeat [])
-   f x = scanl1 (\z n -> DL.map (x:) z ++ n)
+   f x = scanl1 (\z n -> map (x:) z ++ n)
 
 {-|
   The 'propaguleTypes' function generates lists of a given length p of lists of a given length g of type 'Bool'.
@@ -69,7 +69,7 @@ combsWithRep k xs = combsBySize xs !! k
   [[[True,True],[True,True]],[[True,True],[True,False]],[[True,True],[False,True]],[[True,True],[False,False]],[[True,False],[True,False]],[[True,False],[False,True]],[[True,False],[False,False]],[[False,True],[False,True]],[[False,True],[False,False]],[[False,False],[False,False]]]
 -}
 propaguleTypes :: Int -> Int -> [[[Bool]]]
--- propaguleTypes p g = CM.replicateM p (boolLists g)
+-- propaguleTypes p g = replicateM p (boolLists g)
 propaguleTypes p g = combsWithRep p (boolLists g)
 
 {-|
@@ -92,7 +92,7 @@ checkViability xs = all or (transpose xs)
   [[[True,True],[True,True],[True,True]],[[True,True],[True,True],[True,False]],[[True,True],[True,True],[False,True]],[[True,True],[True,True],[False,False]],[[True,True],[True,False],[True,False]],[[True,True],[True,False],[False,True]],[[True,True],[True,False],[False,False]],[[True,True],[False,True],[False,True]],[[True,True],[False,True],[False,False]],[[True,True],[False,False],[False,False]],[[True,False],[True,False],[False,True]],[[True,False],[False,True],[False,True]],[[True,False],[False,True],[False,False]]]
 -}
 viablePropagules :: Int -> Int -> [[[Bool]]]
-viablePropagules p g = DL.filter checkViability (propaguleTypes p g)
+viablePropagules p g = filter checkViability (propaguleTypes p g)
 
 {-|
   The 'countPropTypes' function generates a list containing lists
@@ -104,9 +104,9 @@ viablePropagules p g = DL.filter checkViability (propaguleTypes p g)
   [[3,0,0,0],[2,1,0,0],[2,0,1,0],[2,0,0,1],[1,2,0,0],[1,1,1,0],[1,1,0,1],[1,0,2,0],[1,0,1,1],[1,0,0,2],[0,2,1,0],[0,1,2,0],[0,1,1,1]]
 -}
 countPropTypes :: Int -> Int -> [[Occur]]
-countPropTypes p g = transpose (DL.map (countGenomesInProps p g) (boolLists g))
+countPropTypes p g = transpose (map (countGenomesInProps p g) (boolLists g))
   where
-    countGenomesInProps p g xs = DL.map (occur xs . MS.fromList) (viablePropagules p g)
+    countGenomesInProps p g xs = map (occur xs . fromList) (viablePropagules p g)
 
 {-|
   Standard factorial function
@@ -118,23 +118,23 @@ factorial n = product [n, n-1 .. 1]
   The 'probAGivenB' function generates the conditional unnormalized
   probability weights of transitioning from one propagule type to another.
 
-  >>>let pp = CM.replicateM 2 (countPropTypes 3 2)
+  >>>let pp = replicateM 2 (countPropTypes 3 2)
   [[[2,0,0,0],[2,0,0,0]],[[2,0,0,0],[1,1,0,0]],[[2,0,0,0],[1,0,1,0]],[[2,0,0,0],[1,0,0,1]],[[2,0,0,0],[0,1,1,0]],[[1,1,0,0],[2,0,0,0]],[[1,1,0,0],[1,1,0,0]],[[1,1,0,0],[1,0,1,0]],[[1,1,0,0],[1,0,0,1]],[[1,1,0,0],[0,1,1,0]],[[1,0,1,0],[2,0,0,0]],[[1,0,1,0],[1,1,0,0]],[[1,0,1,0],[1,0,1,0]],[[1,0,1,0],[1,0,0,1]],[[1,0,1,0],[0,1,1,0]],[[1,0,0,1],[2,0,0,0]],[[1,0,0,1],[1,1,0,0]],[[1,0,0,1],[1,0,1,0]],[[1,0,0,1],[1,0,0,1]],[[1,0,0,1],[0,1,1,0]],[[0,1,1,0],[2,0,0,0]],[[0,1,1,0],[1,1,0,0]],[[0,1,1,0],[1,0,1,0]],[[0,1,1,0],[1,0,0,1]],[[0,1,1,0],[0,1,1,0]]]
-  >>>let pab = DL.map probAGivenB pp
+  >>>let pab = map probAGivenB pp
   [4.0,1.0,1.0,1.0,0.0,0.0,2.0,0.0,0.0,0.0,0.0,0.0,2.0,0.0,0.0,0.0,0.0,0.0,2.0,0.0,0.0,0.0,0.0,0.0,2.0]
   >>>let ppab = [x / (sum pab) | x <- pab]
 -}
 probAGivenB :: [[Int]] -> Double
 probAGivenB [a,b] =
-  let x = DL.map fromIntegral a
-      y = DL.map fromIntegral b
-  in factorial (sum x) / product (DL.map factorial x) * product (zipWith (**) y x)
+  let x = map fromIntegral a
+      y = map fromIntegral b
+  in factorial (sum x) / product (map factorial x) * product (zipWith (**) y x)
 probAGivenB _ = error "Must input list with exactly two lists"
 
 probDistAB :: Int -> Int -> [([[Occur]], Double)]
 probDistAB p g =
-  let pp = CM.replicateM 2 (countPropTypes p g)
-      pab = DL.map probAGivenB pp
+  let pp = replicateM 2 (countPropTypes p g)
+      pab = map probAGivenB pp
   in zip pp [x / sum pab | x <- pab]
 
 {-|
@@ -147,36 +147,36 @@ probDistAB p g =
   [[3,0,0,0,0],[2,1,0,0,0],[2,0,1,0,0],[2,0,0,1,0],[2,0,0,0,1],[1,2,0,0,0],[1,1,1,0,0],[1,1,0,1,0],[1,1,0,0,1],[1,0,2,0,0],[1,0,1,1,0],[1,0,1,0,1],[1,0,0,2,0],[1,0,0,1,1],[1,0,0,0,2],[0,3,0,0,0],[0,2,1,0,0],[0,2,0,1,0],[0,2,0,0,1],[0,1,2,0,0],[0,1,1,1,0],[0,1,1,0,1],[0,1,0,2,0],[0,1,0,1,1],[0,1,0,0,2],[0,0,3,0,0],[0,0,2,1,0],[0,0,2,0,1],[0,0,1,2,0],[0,0,1,1,1],[0,0,1,0,2],[0,0,0,3,0],[0,0,0,2,1],[0,0,0,1,2],[0,0,0,0,3]]
 -}
 countPopTypes :: Int -> Int -> Int -> [[Occur]]
-countPopTypes n p g = transpose (DL.map (countPropsInPops n p g) (countPropTypes p g))
+countPopTypes n p g = transpose (map (countPropsInPops n p g) (countPropTypes p g))
   where
-    countPropsInPops n p g xs = DL.map (occur xs . MS.fromList) (combsWithRep n (countPropTypes p g))
+    countPropsInPops n p g xs = map (occur xs . fromList) (combsWithRep n (countPropTypes p g))
 
 probBGivenN :: Int -> Int -> Int -> [([[Occur]], Double)]
 probBGivenN n p g =
   let propt = countPropTypes p g
       pab = probDistAB p g
-      ppab = HT.sliceVertical (length propt) (DL.map snd pab)
+      ppab = sliceVertical (length propt) (map snd pab)
       popt = countPopTypes n p g
       popprob = [(x,y) | x<-popt, y<-ppab]
-      multTupleList (x,y) = zipWith (*) (DL.map fromIntegral x) y
+      multTupleList (x,y) = zipWith (*) (map fromIntegral x) y
       normvec xs = [x / sum xs | x <- xs]
-      probvec = DL.map normvec (HT.sliceVertical (length propt) (DL.map (sum . multTupleList) popprob))
-  in zip (DL.map reverse (sequence [popt,propt])) (concat probvec)
+      probvec = map normvec (sliceVertical (length propt) (map (sum . multTupleList) popprob))
+  in zip (map reverse (sequence [popt,propt])) (concat probvec)
 
 probN'GivenN :: [Double] -> [[Int]] -> Double
 probN'GivenN pan [m',m] =
-  let n' = DL.map fromIntegral m'
-      n = DL.map fromIntegral m
-  in factorial (sum n') / product (DL.map factorial n') * product (zipWith (**) pan n')
+  let n' = map fromIntegral m'
+      n = map fromIntegral m
+  in factorial (sum n') / product (map factorial n') * product (zipWith (**) pan n')
 probN'GivenN _ _ = error "Must input list with exactly two lists"
 
 probDistN'N :: Int -> Int -> Int -> [Double]
 probDistN'N n p g =
   let propt = countPropTypes p g
       popt = countPopTypes n p g
-      pbn = HT.sliceVertical (length propt) (DL.map snd (probBGivenN n p g))
-      nn = DL.map reverse (CM.replicateM 2 popt)
-  in zipWith probN'GivenN (DL.concatMap (replicate (length popt)) pbn) nn
+      pbn = sliceVertical (length propt) (map snd (probBGivenN n p g))
+      nn = map reverse (replicateM 2 popt)
+  in zipWith probN'GivenN (concatMap (replicate (length popt)) pbn) nn
 
 -- disp :: Matrix Double -> IO ()
 -- disp = putStr . dispf 2
